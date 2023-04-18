ac4d8c9c87b96707a85292af212af0657a07e67c
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2023-04-18 15:20:10 +0100

    Added new project

--- src/components/About.js
+++ src/components/About.js
@@ -20,15 +20,15 @@ export function About(props){
                 </h2>
 
                 <p className="about__p">
-                    Wrote <span className="about__spotlight">'Hello world' in 2019</span> using <span className="about__spotlight">python.</span> Went from there to learn and use the python's <span className="about__spotlight">tkinter</span> and <span className="about__spotlight">Django</span> frameworks in 2020.
+                    Wrote <span className="about__spotlight">'Hello world' in 2020</span> using <span className="about__spotlight">python.</span> Went from there to learn and use the python's <span className="about__spotlight">Django</span> framework in 2020.
                 </p>
 
                 <p className="about__p">
-                     Worked at GTO in august 2021 as a python(django) developer intern. So far in 2022 I've attended two bootcamps where I've been able to horn my <span className="about__spotlight">HTML</span> & <span className="about__spotlight">CSS</span> skill, learn and work with <span className="about__spotlight">JavaScript(ES6+)</span> and also <span className="about__spotlight">React</span>
+                     Worked at GTO in august 2021 as a python developer intern. I attended Classvisa bootcamp in 2022 where I've been able to horn my <span className="about__spotlight">HTML</span> & <span className="about__spotlight">CSS</span> skill, learnt and work with <span className="about__spotlight">JavaScript(ES6+), Nodejs</span> and also <span className="about__spotlight">React</span>
                 </p>
 
                 <p className="about__p">
-                     I'm a part of WIP - community where junior developers work together to build real life projects. I'm also a student at <a href="http://classvisa.com"> <span> Classvisa </span> </a>
+                     I'm a part of WIP - a community where junior developers work together to build real life projects. I currently work as a backend developer at FountianPay but I'm <span className="about__spotlight">open to other opportunities</span>
                 </p>
 
             </div>
--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,19 +1,31 @@
 const ProjectData = [
-    
+    {
+        title : 'Jobhive',
+        description : 'A webapp where authenticated users can create new jobs post, update and also delete it.',
+        gitlink : 'https://jobhive.cyclic.app/jobs',
+        livelink : '',
+        tools : 'Nodejs| Express-react | Passpoerjs | Cyclic '
+    },
     {
         title : 'Airbnb Clone',
         description : 'A web app simulating the appearance and some the feature of the airbnb website',
         gitlink : 'https://github.com/JosephBusayo/airbnb-clone',
-        livelink : 'https://airbnb-clone-659c5.web.app/',
-        tools : 'React Firebase'
+        livelink : 'https://airbnbclone-app.web.app/',
+        tools : 'React | Firebase'
     },
-
     {
-        title : 'House Of Pages',
-        description : 'An online platformm that allow users access books from the google book API ',
-        gitlink : 'https://github.com/JosephBusayo/houseofpages',
+        title : 'Wordy',
+        description : 'A website that accepts combination of English letters as input and outputs every other word that can be formed from it.',
+        gitlink : 'https://github.com/JosephBusayo/AnagramWeb',
         livelink : '',
-        tools : 'Python Django JavaScript'
+        tools : 'Python | Django '
+    },
+    {
+        title : 'Questo',
+        description : 'A task management app with the drag and drop feature ',
+        gitlink : 'https://github.com/JosephBusayo/questo',
+        livelink : 'https://quest-task.web.app/',
+        tools : 'React | Firebase'
     },
 
     {
@@ -21,16 +33,8 @@ const ProjectData = [
         description : 'A fun filled click-a-mole game ',
         gitlink : 'https://github.com/JosephBusayo/click_a_mole',
         livelink : 'https://josephbusayo.github.io/click_a_mole/',
-        tools : 'React Firebase'
+        tools : 'React | Firebase'
     },
-    {
-        title : 'Wordy',
-        description : 'A website that accepts combination of English letters as input and outputs every other word that can be formed from it.',
-        gitlink : 'https://github.com/JosephBusayo/AnagramWeb',
-        livelink : '',
-        tools : 'Python Django Fly '
-    },
-
 
 ]
 
